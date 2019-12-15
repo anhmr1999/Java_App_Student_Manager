3e828703c8e2f58205eb69f3756f20e5049825ef
anhmr1999 <mr.anhnh1999@gmail.com>
2019-12-15 16:33:10 +0700

    edit db

--- App_Manager/DB/Database.sql
+++ App_Manager/DB/Database.sql
@@ -9,14 +9,15 @@ go
 create table tbl_Course(
 	ID int identity primary key,
 	Name nvarchar(100) not null,
-	Begin_date date,
+	Begin_date date default(GETDATE()),
 	End_date date
 )
 GO
 
-create table tbl_Role(
+create table tbl_Class(
 	ID int identity primary key,
-	Name nvarchar(100)
+	Name nvarchar(100),
+	Course_ID int foreign key references tbl_Course(ID)
 )
 GO
 
@@ -28,65 +29,55 @@ create table tbl_Student(
 	Email varchar(100) unique,
 	Address nvarchar(100),
 	DOB date default('2000/01/01'),
+	img nvarchar(100),
 	Status tinyint default(1), /* 1 - đang học, 2 - đang bảo lưu, 3 - đã thôi học, 4 - đã tốt nghiệp */
-	Course int foreign key references tbl_Course(ID)
+	Class_ID int foreign key references tbl_Class(ID)
+)
+GO
+
+create table tbl_Role(
+	ID int identity primary key,
+	Name nvarchar(100)
 )
 GO
 
 create table tbl_Teacher(
 	ID int identity primary key,
-	Name nvarchar(100) not null,
-	Phone nvarchar(10) unique,
-	Email varchar(100) unique,
+	Name nvarchar(100),
+	Phone nvarchar(11),
+	Email nvarchar(100),
+	Password nvarchar(100),
 	Address nvarchar(100),
-	Password varchar(100) not null,
-	DOB date,
-	Status tinyint default(1), /* 1- đang dạy học, 2- đã về hưu */
-	Lever_role int foreign key references tbl_Role(ID)
+	DOB date default('1980/01/01'),
+	Status tinyint default(1), /* 1- đang giảng dạy, 2- đã về hưu */
+	Role_ID int foreign key references tbl_Role(ID)
 )
 GO
 
 create table tbl_Subject(
 	ID int identity primary key,
-	Name nvarchar(100) not null,
-	Teacher int foreign key references tbl_Teacher(ID)
+	Name nvarchar(100)
 )
 GO
 
-create table tbl_Class(
+create table tbl_Teach_Sub(
 	ID int identity primary key,
-	Name nvarchar(100) not null,
-	Max_Student int default(50),
-	Subject int foreign key references tbl_Subject(ID)
-)
-GO
-
-create table Class_Student(
-	Student_ID int foreign key references tbl_Student(ID),
-	Class_ID int foreign key references tbl_Class(ID)
+	Teacher_ID int foreign key references tbl_Teacher(ID),
+	Subject_ID int foreign key references tbl_Subject(ID)
 )
 GO
 
 create table tbl_Mark(
 	Student_ID int foreign key references tbl_Student(ID),
-	Subject_ID int foreign key references tbl_Subject(ID),
-	Mark float,
-	Exam_times int default(1),
-	Status int default(1), /* 1 - chưa thi- ko hiển thị, 2 - đã thi, đang tính điểm, 3 - cấm thi, 4 - trượt, 5 - qua môn */
+	Sub_ID int foreign key references tbl_Teach_Sub(ID),
+	Mark float default(0),
 	Note nvarchar(500)
 )
 GO
 
-create table Student_Subject(
-	Student_ID int foreign key references tbl_Student(ID),
-	Subject_ID int foreign key references tbl_Subject(ID)
-)
-GO
-
-create table tbl_History(
-	ID int identity primary key,
-	Teacher int foreign key references tbl_Teacher(ID),
-	Changer nvarchar(100),
-	Time_change datetime default(GETDATE())
-)
+insert into tbl_Role values
+(N'Hiệu trưởng'),
+(N'Phó hiệu trưởng'),
+(N'Giảng viên'),
+(N'Giảng viên thực tập')
 GO
